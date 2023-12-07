e9dc2c6e448171f83fd5a564adbb0e00a4eb18f6
Simon Eveille <eveillesimon@gmail.com>
2023-12-07 11:47:55 +0100

    First commit, initialize blank springboot project using jdk21

---
+++ readme.md
@@ -0,0 +1,5 @@
+# Module communication-api
+
+Backend pour le module de communication du projet tutoré, pour l'UV TA75
+
+##ins
---
+++ src/test/java/com/utbm/fisa/communicationapi/CommunicationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utbm.fisa.communicationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunicationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
