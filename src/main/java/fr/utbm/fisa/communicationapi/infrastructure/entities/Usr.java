7a85e8e63c6ae768f020734a6a26533b71ac9af4
Léo.T <leotoul@hotmail.fr>
2023-12-18 17:46:28 +0100

    Modification de la classe Usr, en fonction de la table groupe inscription

---
+++ src/main/java/fr/utbm/fisa/communicationapi/infrastructure/entities/Usr.java
@@ -0,0 +1,140 @@
+package fr.utbm.fisa.communicationapi.infrastructure.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Usr {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    private String email;
+
+    private Timestamp createdAt;
+
+    private Timestamp updatedAt;
+
+    private Timestamp deletedAt;
+
+    private Boolean isAdmin;
+
+    @Enumerated(EnumType.STRING)
+    private UserType userType;
+
+    private String profilePicture;
+
+    @OneToMany(mappedBy = "sender")
+    private List<Communication> sentCommunications;
+
+    public enum UserType{
+        Parent,
+        Staff
+    }
+
+    public Usr(Long id, String username, String password, String email, Timestamp createdAt, Timestamp updatedAt, Timestamp deletedAt, Boolean isAdmin, UserType userType, String profilePicture) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.deletedAt = deletedAt;
+        this.isAdmin = isAdmin;
+        this.userType = userType;
+        this.profilePicture = profilePicture;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Timestamp getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Timestamp updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Timestamp getDeletedAt() {
+        return deletedAt;
+    }
+
+    public void setDeletedAt(Timestamp deletedAt) {
+        this.deletedAt = deletedAt;
+    }
+
+    public Boolean getAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(Boolean admin) {
+        isAdmin = admin;
+    }
+
+    public UserType getUserType() {
+        return userType;
+    }
+
+    public void setUserType(UserType userType) {
+        this.userType = userType;
+    }
+
+    public String getProfilePicture() {
+        return profilePicture;
+    }
+
+    public void setProfilePicture(String profilePicture) {
+        this.profilePicture = profilePicture;
+    }
+}
