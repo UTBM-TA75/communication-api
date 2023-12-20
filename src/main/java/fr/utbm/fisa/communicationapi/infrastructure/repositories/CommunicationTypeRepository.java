e5a700ef2b733a5a634779e26fcdd26ff1139c36
BaptisteFanjoux <Baptiste.fanjoux@outlook.fr>
2023-12-20 17:34:27 +0100

    Suppression de ancien communicationType

--- src/main/java/fr/utbm/fisa/communicationapi/infrastructure/repositories/CommunicationTypeRepository.java
+++
@@ -1,7 +0,0 @@
-package fr.utbm.fisa.communicationapi.infrastructure.repositories;
-
-import fr.utbm.fisa.communicationapi.infrastructure.entities.CommunicationType;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CommunicationTypeRepository extends CrudRepository<CommunicationType, Integer> {
-}
