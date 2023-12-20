969a0d5fc03ad4e356ee9064c5a5d515dc3056dd
BaptisteFanjoux <Baptiste.fanjoux@outlook.fr>
2023-12-20 17:33:03 +0100

    Suppression de group

--- src/main/java/fr/utbm/fisa/communicationapi/infrastructure/repositories/GroupRepository.java
+++
@@ -1,8 +0,0 @@
-package fr.utbm.fisa.communicationapi.infrastructure.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import fr.utbm.fisa.communicationapi.infrastructure.entities.Group;
-
-public interface GroupRepository extends CrudRepository<Group, Long> {
-
-}
