798423c00aea88103286b726c0484834cc2abad4
BaptisteFanjoux <Baptiste.fanjoux@outlook.fr>
2023-12-21 15:52:14 +0100

    Creation de poll_answerController

--- src/main/java/fr/utbm/fisa/communicationapi/infrastructure/repositories/Poll_answerRepository.java
+++ src/main/java/fr/utbm/fisa/communicationapi/infrastructure/repositories/Poll_answerRepository.java
@@ -3,5 +3,5 @@ package fr.utbm.fisa.communicationapi.infrastructure.repositories;
 import fr.utbm.fisa.communicationapi.infrastructure.entities.Poll_answer;
 import org.springframework.data.repository.CrudRepository;
 
-public interface Poll_answerRepository extends CrudRepository<Poll_answer, Long> {
+public interface Poll_answerRepository extends CrudRepository<Poll_answer, Integer> {
 }
