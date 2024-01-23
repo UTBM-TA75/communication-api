fc56c07c859e8a5486f7a3be567684c792ed7421
Théo DURR <git@theodurr.fr>
2024-01-23 23:37:12 +0100

    feat(discussions): edit path of discussions to use rest verbs

--- src/main/java/fr/utbm/fisa/communicationapi/exposition/controller/DiscussionController.java
+++ src/main/java/fr/utbm/fisa/communicationapi/exposition/controller/DiscussionController.java
@@ -6,7 +6,10 @@ import fr.utbm.fisa.communicationapi.infrastructure.entities.Message;
 import fr.utbm.fisa.communicationapi.infrastructure.repositories.DiscussionRepository;
 import fr.utbm.fisa.communicationapi.infrastructure.repositories.MessageRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,63 +21,100 @@ import java.util.List;
 public class DiscussionController {
     private final DiscussionRepository discussionRepository;
     private final MessageRepository messageRepository;
+
+    /**
+     * Gets all the discussions
+     *
+     * @return the list of discussions
+     */
     @GetMapping("/discussions")
-    public Iterable<Discussion> get() {
-        return discussionRepository.findAll();
+    public ResponseEntity<Iterable<Discussion>> getAllDiscussions() {
+        return ResponseEntity.ok(discussionRepository.findAll());
     }
 
-    @PostMapping("/discussions/create")
-    public Discussion create(@RequestBody Discussion discussion) {
+    /**
+     * Get a discussion with the given id
+     *
+     * @param id the discussion's id
+     * @return the discussion
+     */
+    @GetMapping("/discussion/{id}")
+    public ResponseEntity<Discussion> getDiscussion(@PathVariable int id) {
+        Discussion d = discussionRepository
+                .findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "No discussion found with the id " + id));
+
+        return ResponseEntity.ok(d);
+    }
+
+    /**
+     * Creates a discussion
+     *
+     * @param discussion the discussion to create
+     * @return the created discussion
+     */
+    @PostMapping("/discussions")
+    public ResponseEntity<Discussion> createDiscussion(@RequestBody Discussion discussion) {
+        // TODO: Add validation to test if the discussion between theses two users already exists
         Discussion disc = new Discussion();
         disc.setUser1(discussion.getUser1());
         disc.setUser2(discussion.getUser2());
         disc.setDiscussionList(discussion.getDiscussionList());
-        return discussionRepository.save(disc);
+
+        discussionRepository.save(disc);
+
+        return new ResponseEntity<>(disc, HttpStatus.CREATED);
     }
 
-    @GetMapping("/discussions/delete")
-    public void delete(@RequestBody Discussion discussion) {
-        discussionRepository.deleteById(discussion.getId());
+    /**
+     * Deletes the discussion with the given id
+     *
+     * @param id the discussion id
+     */
+    @DeleteMapping("/discussions/{id}")
+    public void deleteDiscussion(@PathVariable int id) {
+        Discussion d = discussionRepository
+                .findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "No discussion found with id " + id));
+
+        discussionRepository.deleteById(id);
     }
 
-    @PostMapping("/discussions/edit")
-    public void edit(@RequestBody Discussion discussion) {
-        discussionRepository.findById(discussion.getId()).map(
-                disc -> {
-                    disc.setUser1(discussion.getUser1());
-                    disc.setUser2(discussion.getUser2());
-                    disc.setDiscussionList(discussion.getDiscussionList());
-                    return discussionRepository.save(disc);
-                }
-        );
-    }
-
+    // TODO: comment me @Simon Eveillé
     @GetMapping("/discussions/preview")
-    public Iterable<HomeDiscussionPreview> getPreview(@RequestParam Long usrId) {
+    public ResponseEntity<Iterable<HomeDiscussionPreview>> getPreview(@RequestParam Long usrId) {
         ArrayList<HomeDiscussionPreview> previews = new ArrayList<>();
 
         List<Discussion> discussions = discussionRepository.findByUsrId(usrId);
-        for (Discussion d: discussions) {
+        for (Discussion d : discussions) {
             Message lastMessage = messageRepository.findByDiscussionId(d.getId());
             String otherUser = d.getUser1().getId().equals(usrId) ? d.getUser2().getUsername() : d.getUser1().getUsername();
             int unseenCount = discussionRepository.countUnseenMessages(d.getId());
 
             previews.add(
-                HomeDiscussionPreview.builder()
-                    .name(otherUser)
-                    .message(lastMessage.getBody())
-                    .unseenCount(unseenCount)
-                .build()
+                    HomeDiscussionPreview.builder()
+                            .name(otherUser)
+                            .message(lastMessage.getBody())
+                            .unseenCount(unseenCount)
+                            .build()
             );
         }
 
-        return previews;
+        return ResponseEntity.ok(previews);
     }
 
-    @GetMapping("/discussions/messages")
-    public Iterable<Message> getMessages(@RequestParam int discussionId){
-        Discussion discussion = discussionRepository.findByDiscussionId(discussionId);
-        return discussionRepository.getMessages(discussion);
-    }
+    /**
+     * Get all the messages of a discussion
+     *
+     * @param id the discussion id
+     * @return the message list of the discussion
+     */
+    @GetMapping("/discussions/{id}/messages")
+    public ResponseEntity<Iterable<Message>> getMessages(@PathVariable int id) {
+        Discussion discussion = discussionRepository
+                .findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "No discussion with id " + id));
 
+        return ResponseEntity.ok(discussionRepository.getMessages(id));
+    }
 }
--- src/main/java/fr/utbm/fisa/communicationapi/infrastructure/repositories/DiscussionRepository.java
+++ src/main/java/fr/utbm/fisa/communicationapi/infrastructure/repositories/DiscussionRepository.java
@@ -13,15 +13,11 @@ public interface DiscussionRepository extends CrudRepository<Discussion, Integer
             nativeQuery = true)
     List<Discussion> findByUsrId(@Param("usrId") Long userId);
 
-    @Query(value = "SELECT * FROM discussion d WHERE d.id = :discussionId",
-            nativeQuery = true)
-    Discussion findByDiscussionId(@Param("discussionId") int discussionId);
-
     @Query(value = "SELECT COUNT(*) FROM message m WHERE m.idDiscussion = :idDiscussion AND m.seen =  0",
             nativeQuery = true)
     Integer countUnseenMessages(@Param("idDiscussion") int idDiscussion);
 
     @Query(value = "SELECT * FROM message m WHERE m.idDiscussion = :discussionId",
             nativeQuery = true)
-    List<Message> getMessages(@Param("discussionId") Discussion discussionId);
+    List<Message> getMessages(@Param("discussionId") int discussionId);
 }
