d0e13b7d942425f2720c41ab6087a3c8a2f63d52
Théo DURR <git@theodurr.fr>
2024-01-24 10:12:36 +0100

    feat(messages): edit path of messages to use rest verbs

--- src/main/java/fr/utbm/fisa/communicationapi/exposition/controller/MessageController.java
+++ src/main/java/fr/utbm/fisa/communicationapi/exposition/controller/MessageController.java
@@ -4,7 +4,10 @@ import fr.utbm.fisa.communicationapi.infrastructure.entities.Message;
 import fr.utbm.fisa.communicationapi.infrastructure.entities.Pupil;
 import fr.utbm.fisa.communicationapi.infrastructure.repositories.MessageRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 
 @RestController
@@ -12,40 +15,61 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class MessageController {
     private final MessageRepository messageRepository;
-    @GetMapping("/messages")
-    public Iterable<Message> get() {
-        return messageRepository.findAll();
+
+    /**
+     * Create a message
+     *
+     * @param data the message's data to create
+     * @return the created message
+     */
+    @PostMapping("/messages")
+    public ResponseEntity<Message> createMessage(@RequestBody Message data) {
+        Message message = new Message();
+        message.setIdSender(data.getIdSender());
+        message.setIdDiscussion(data.getIdDiscussion());
+        message.setBody(data.getBody());
+
+        messageRepository.save(message);
+
+        return new ResponseEntity<>(message, HttpStatus.CREATED);
     }
 
-    @PostMapping("/messages/create")
-    public Message create(@RequestBody Message message) {
-        Message mess = new Message();
-        mess.setIdSender(message.getIdSender());
-        mess.setIdDiscussion(message.getIdDiscussion());
-        mess.setBody(message.getBody());
-        mess.setSentDate(message.getSentDate());
-        mess.setSeen(message.getSeen());
-        mess.setSeenDate(message.getSeenDate());
-        return messageRepository.save(mess);
-    }
+    /**
+     * Deletes a message
+     *
+     * @param id the message's id
+     */
+    @GetMapping("/messages/{id}")
+    public void delete(@PathVariable int id) {
+        Message message = messageRepository
+                .findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "No message found with id " + id));
 
-    @GetMapping("/messages/delete")
-    public void delete(@RequestBody Message message) {
         messageRepository.deleteById(message.getId());
     }
 
-    @PostMapping("/messages/edit")
-    public void edit(@RequestBody Message message) {
-        messageRepository.findById(message.getId()).map(
-                mess -> {
-                    mess.setIdSender(message.getIdSender());
-                    mess.setIdDiscussion(message.getIdDiscussion());
-                    mess.setBody(message.getBody());
-                    mess.setSentDate(message.getSentDate());
-                    mess.setSeen(message.getSeen());
-                    mess.setSeenDate(message.getSeenDate());
-                    return messageRepository.save(mess);
-                }
-        );
+    /**
+     * Updates a message
+     *
+     * @param id   the message's id
+     * @param data the message's updated data
+     * @return the updated message
+     */
+    @PutMapping("/messages/{id}")
+    public ResponseEntity<Message> edit(@PathVariable int id, @RequestBody Message data) {
+        Message message = messageRepository
+                .findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "No message found with id " + id));
+
+        message.setIdSender(data.getIdSender());
+        message.setIdDiscussion(data.getIdDiscussion());
+        message.setBody(data.getBody());
+        message.setSentDate(data.getSentDate());
+        message.setSeen(data.getSeen());
+        message.setSeenDate(data.getSeenDate());
+
+        messageRepository.save(message);
+
+        return ResponseEntity.ok(message);
     }
 }
