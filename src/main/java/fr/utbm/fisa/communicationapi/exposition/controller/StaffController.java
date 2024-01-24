83f9fa48faa5c9dc2802fde81612b852c38b30c3
Théo DURR <git@theodurr.fr>
2024-01-24 13:10:32 +0100

    feat(staff): edit path of staff members to use rest verbs

--- src/main/java/fr/utbm/fisa/communicationapi/exposition/controller/StaffController.java
+++ src/main/java/fr/utbm/fisa/communicationapi/exposition/controller/StaffController.java
@@ -3,7 +3,10 @@ package fr.utbm.fisa.communicationapi.exposition.controller;
 import fr.utbm.fisa.communicationapi.infrastructure.entities.Staff;
 import fr.utbm.fisa.communicationapi.infrastructure.repositories.StaffRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 
 @RestController
@@ -11,38 +14,81 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class StaffController {
     private final StaffRepository staffRepository;
-    @GetMapping("/staffs")
-    public Iterable<Staff> get() {
-        return staffRepository.findAll();
+
+    /**
+     * Gets all staff members
+     *
+     * @return the list of staff members
+     */
+    @GetMapping("/staff-members")
+    public ResponseEntity<Iterable<Staff>> getAllStaffMembers() {
+        return ResponseEntity.ok(staffRepository.findAll());
     }
 
-    @PostMapping("/staffs/create")
-    public Staff create(@RequestBody Staff staff) {
-        Staff stff = new Staff();
-        stff.setIdUser(staff.getIdUser());
-        stff.setLastName(staff.getLastName());
-        stff.setFirstName(staff.getFirstName());
-        stff.setSenderList(staff.getSenderList());
-        stff.setTeacherList(staff.getTeacherList());
-        return staffRepository.save(stff);
+    /**
+     * Get staff member
+     *
+     * @param id the staff member's id
+     * @return the staff member
+     */
+    @GetMapping("/staff-members/{id}")
+    public ResponseEntity<Staff> getStaffMember(@PathVariable Long id) {
+        Staff staff = staffRepository
+                .findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "No staff member found with id " + id));
+
+        return ResponseEntity.ok(staff);
     }
 
-    @GetMapping("/staffs/delete")
-    public void delete(@RequestBody Staff staff) {
+    /**
+     * Creates a staff member
+     *
+     * @param data the staff member's data
+     * @return the created staff member
+     */
+    @PostMapping("/staff-members")
+    public ResponseEntity<Staff> createStaffMember(@RequestBody Staff data) {
+        Staff staff = new Staff();
+        staff.setIdUser(data.getIdUser());
+        staff.setLastName(data.getLastName());
+        staff.setFirstName(data.getFirstName());
+
+        staffRepository.save(staff);
+
+        return new ResponseEntity<>(staff, HttpStatus.CREATED);
+    }
+
+    /**
+     * Deletes a staff member
+     * @param id the staff member's id
+     */
+    @DeleteMapping("/staff-members/{id}")
+    public void deleteStaffMember(@PathVariable Long id) {
+        Staff staff = staffRepository
+                .findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "No staff member found with id " + id));
+
+
         staffRepository.deleteById(staff.getId());
     }
 
-    @PostMapping("/staffs/edit")
-    public void edit(@RequestBody Staff staff) {
-        staffRepository.findById(staff.getId()).map(
-                stff -> {
-                    stff.setIdUser(staff.getIdUser());
-                    stff.setLastName(staff.getLastName());
-                    stff.setFirstName(staff.getFirstName());
-                    stff.setSenderList(staff.getSenderList());
-                    stff.setTeacherList(staff.getTeacherList());
-                    return staffRepository.save(stff);
-                }
-        );
+    /**
+     * Updates a staff member
+     * @param id the staff member's id
+     * @param data the staff member's updated data
+     * @return the updated staff member
+     */
+    @PutMapping("/staff-members/{id}")
+    public ResponseEntity<Staff> editStaffMember(@PathVariable Long id, @RequestBody Staff data) {
+        Staff staff = staffRepository
+                .findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "No staff member found with id " + id));
+
+        staff.setLastName(data.getLastName());
+        staff.setFirstName(data.getFirstName());
+
+        staffRepository.save(staff);
+
+        return ResponseEntity.ok(staff);
     }
 }
