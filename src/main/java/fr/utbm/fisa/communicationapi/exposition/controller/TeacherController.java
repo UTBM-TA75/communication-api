8a6f6719377f18e69079a472e58e68e1ef26c461
Théo DURR <git@theodurr.fr>
2024-01-24 13:18:56 +0100

    feat(teacher): edit path of teacher members to use rest verbs

--- src/main/java/fr/utbm/fisa/communicationapi/exposition/controller/TeacherController.java
+++ src/main/java/fr/utbm/fisa/communicationapi/exposition/controller/TeacherController.java
@@ -3,7 +3,10 @@ package fr.utbm.fisa.communicationapi.exposition.controller;
 import fr.utbm.fisa.communicationapi.infrastructure.entities.Teacher;
 import fr.utbm.fisa.communicationapi.infrastructure.repositories.TeacherRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 
 @RestController
@@ -11,36 +14,71 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class TeacherController {
     private final TeacherRepository teacherRepository;
+
+    /**
+     * Gets all teachers
+     *
+     * @return the list of teachers
+     */
     @GetMapping("/teachers")
-    public Iterable<Teacher> get() {
-        return teacherRepository.findAll();
+    public ResponseEntity<Iterable<Teacher>> get() {
+        return ResponseEntity.ok(teacherRepository.findAll());
     }
 
-    @PostMapping("/teachers/create")
-    public Teacher create(@RequestBody Teacher teacher) {
-        Teacher teachr = new Teacher();
-        teachr.setIdStaff(teacher.getIdStaff());
-        teachr.setStartDate(teacher.getStartDate());
-        teachr.setEndDate(teacher.getEndDate());
-        teachr.setTeacherClasses(teacher.getTeacherClasses());
-        return teacherRepository.save(teachr);
+    /**
+     * Gets a teacher
+     *
+     * @param id the teacher's id
+     * @return the teacher
+     */
+    @GetMapping("/teachers/{id}")
+    public ResponseEntity<Teacher> getTeacher(@PathVariable Long id) {
+        Teacher teacher = teacherRepository
+                .findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "No teacher found with id " + id));
+
+        return ResponseEntity.ok(teacher);
     }
 
-    @GetMapping("/teachers/delete")
-    public void delete(@RequestBody Teacher teacher) {
+    /**
+     * Creates a teacher
+     *
+     * @param data the teacher's data
+     * @return the created teacher
+     */
+    @PostMapping("/teachers")
+    public ResponseEntity<Teacher> create(@RequestBody Teacher data) {
+        Teacher teacher = new Teacher();
+        teacher.setIdStaff(data.getIdStaff());
+        teacher.setStartDate(data.getStartDate());
+        teacher.setEndDate(data.getEndDate());
+
+        teacherRepository.save(teacher);
+
+        return new ResponseEntity<>(teacher, HttpStatus.CREATED);
+    }
+
+
+    @DeleteMapping("/teachers/{id}")
+    public void delete(@PathVariable Long id) {
+        Teacher teacher = teacherRepository
+                .findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "No teacher found with id " + id));
+
         teacherRepository.deleteById(teacher.getId());
     }
 
-    @PostMapping("/teachers/edit")
-    public void edit(@RequestBody Teacher teacher) {
-        teacherRepository.findById(teacher.getId()).map(
-                teachr -> {
-                    teachr.setIdStaff(teacher.getIdStaff());
-                    teachr.setStartDate(teacher.getStartDate());
-                    teachr.setEndDate(teacher.getEndDate());
-                    teachr.setTeacherClasses(teacher.getTeacherClasses());
-                    return teacherRepository.save(teachr);
-                }
-        );
+    @PutMapping("/teachers/{id}")
+    public ResponseEntity<Teacher> edit(@PathVariable Long id, @RequestBody Teacher data) {
+        Teacher teacher = teacherRepository
+                .findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "No teacher found with id " + id));
+
+        teacher.setStartDate(data.getStartDate());
+        teacher.setEndDate(data.getEndDate());
+
+        teacherRepository.save(teacher);
+
+        return ResponseEntity.ok(teacher);
     }
 }
