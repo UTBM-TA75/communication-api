f8f195aa9828c9a85fb18122cf6cab7f4d665b8a
Théo DURR <git@theodurr.fr>
2024-02-07 16:04:37 +0100

    chore: updated cors config

--- src/main/java/fr/utbm/fisa/communicationapi/exposition/CommunicationApiApplication.java
+++ src/main/java/fr/utbm/fisa/communicationapi/exposition/CommunicationApiApplication.java
@@ -25,7 +25,8 @@ public class CommunicationApiApplication {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("*")
-						.allowedOrigins("http://localhost:4200");
+						.allowedOrigins("http://localhost:4200")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
 			}
 		};
 	}
